a9528d3805d15625a87b0b3195613d9d0b651d11
abhaykes1 <abhaykesharwani40@gmail.com>
2020-11-12 01:36:32 +0530

    added functions for each operation on tender

--- Main/transparent-fund/client/src/App.js
+++ Main/transparent-fund/client/src/App.js
@@ -1,75 +1,106 @@
-import React, { Component } from "react";
-import ElectionContract from "./contracts/Election.json";
+import React,{useState, useEffect}  from "react";
 import StorageContract from "./contracts/SimpleStorage.json";
 
-import getWeb3 from "./getWeb3";
 import Web3 from 'web3';
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: "", web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // await this.onSubmit()
-      // // Get network provider and web3 instance.
-      // const web3 = await getWeb3();
 
-      // // Use web3 to get the user's accounts.
-      // const accounts = await web3.eth.getAccounts();
+const App = () => {
+  const [val, setVal] = useState(null)
+  const [electionName, setElectionName] = useState("");
+  const [authorize, setAuthorize] = useState("");
+  const [candidate, setCandidate] = useState("");
+  const [accounts, setAccounts] = useState(null)
+  const [contract, setContract] = useState(null)
+  const [num, setNum] = useState(0)
+  const [id, setId] = useState(0)
+  const [allCandidates, setAllCandidates] = useState([])
 
-      // // Get the contract instance.
-      // const networkId = await web3.eth.net.getId();
-      // const deployedNetwork = SimpleStorageContract.networks[networkId];
-      // console.log('asasa',deployedNetwork)
-
-      // const instance = new web3.eth.Contract(
-      //   SimpleStorageContract.abi,
-      //   deployedNetwork && deployedNetwork.address,
-      // );
-      // // Set web3, accounts, and contract to the state, and then proceed with an
-      // // example of interacting with the contract's methods.
-      // this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+  const _setElectionNameHelper = async() => {
+    if(!accounts){
+      alert('account not set');
+      return;
     }
-  };
+    if(!contract){
+      alert('contract not set');
+      return;
+    }
+    if(!electionName){
+      alert('set name please!')
+      return;
+    }
+    await contract.methods.setElectionName(electionName).send({ from: accounts });
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-    console.log('asa',accounts)
-
-    // Stores a given value, 5 by default.
-    await contract.methods.setElectionName("asssssssssssssss").send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
     const response = await contract.methods.getElectionName().call();
 
-    await contract.methods.authorize("0x78b71e4281bdb468e2754045968a1438f041ed57").send({from: accounts[0]})
-    await contract.methods.addCandidate("Abhay Kesharwani").send({from: accounts[0]})
+    setVal(response);
 
-    await contract.methods.getNumCandidate().call((err, res) => {
-      console.log('res',res)
-    })
-    await contract.methods.candidates(0).call((err, res) => {
-      console.log('res1',res)
-    })
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-  onSubmit = async (event) => 
-        {
+  }
+  const _setAuthorizeNameHelper = async() => {
+    try {
+      const res = await contract.methods.authorize(authorize).send({from: accounts})
+      console.log('ress',res);
+    } catch (error) {
+      alert(error.reason)
+    }
+  }
+  const _setCandidateNameHelper = async() => {
+    if(!candidate){
+      alert('kuch daal to');
+      return;
+    }
+    try {
+      const res = await contract.methods.addCandidate(candidate).send({from: accounts})
+      console.log('resss',res);
+    } catch (error) {
+      alert(error.reason)
+    }
+  }
+
+  useEffect(() => {
+    async function getInfo(){
+      if(contract && accounts){
+        await contract.methods.getNumCandidate().call((err, res) => {
+          console.log('resssss',res)
+          setNum(res);
+        })
+      }
+    }
+    getInfo()
+  }, [accounts, contract])
+
+  useEffect(() => {
+    async function setNames(){
+      let arr = [];
+      for (let index = 0; index < num; index++) {
+        await contract.methods.candidates(index).call((err, res) => {
+          // console.log(res);
+          arr.push(res);
+        })
+      }
+      setAllCandidates(arr);
+      console.log(allCandidates)
+    }
+    setNames();
+  }, [num])
+
+  const _getCandidateInfo = async () => {
+    try {
+      await contract.methods.candidates(id).call((err, res) => {
+        alert(`name: ${res.name}  votes: ${res.voteCount}`);
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const onSubmit = async () => {
           console.log('clicked') 
-          event.preventDefault();
           window.ethereum.enable()
           //setting up web3 to talk to meta mask
           const web3 = new Web3(Web3.givenProvider);
-          const accounts = await web3.eth.getAccounts()
-          console.log('accounts', accounts[0])
+          const acc = await web3.eth.getAccounts()
+          console.log('accounts', acc[0])
           const deployedNetwork = StorageContract.networks["5777"];
 
           // TODO : call constructor when instance is created 
@@ -77,46 +108,75 @@ class App extends Component {
             StorageContract.abi,
             deployedNetwork && deployedNetwork.address
           );
+          setAccounts(acc[0])
+          setContract(instance)
+  };  
+  useEffect(() => {
+    async function intialize(){
+      await onSubmit();
+    }
+    intialize();
+  }, [])
+  return(
+    <div className='App'>
+      <h2>Election name is: {val}</h2>
+      <h2>total candidates are: {num}</h2>
+      <input value={electionName} onChange={(e)=>{setElectionName(e.target.value)}} type="text" id="electionName" />
+      <button onClick={_setElectionNameHelper} >set election name</button>
+      <br/><br/>
+      <input value={authorize} onChange={(e)=>{setAuthorize(e.target.value)}} type="text" id="authAddr" />
+      <button onClick={_setAuthorizeNameHelper} >authorize address</button>
+      <br/><br/>
+      <input value={candidate} onChange={(e)=>{setCandidate(e.target.value)}} type="text" id="candName" />
+      <button onClick={_setCandidateNameHelper} >add candidate</button>
+      <br/><br/>
+      <input value={id} onChange={(e)=>{setId(e.target.value)}} type="number" id="id" />
+      <button onClick={_getCandidateInfo} >get Info</button>
+      <br/><br/>
 
-          // Set web3, accounts, and contract to the state, and then proceed with an
-          // example of interacting with the contract's methods.
-          this.setState({ web3, accounts, contract: instance });
-            console.log('state', this.state)
-        };  
-  render() {
-    // if (!this.state.accounts) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
-    return (
-      <div className="App">
-        <button onClick={this.onSubmit}>
-         hello
-       </button>
-       <button onClick={this.runExample}>
-         send
-       </button>
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-    // return(
-    //   <div className='container'>
-    //     <button onClick={this.onSubmit}>
-    //       hello
-    //     </button>
-    //     asasas
-    //   </div>
-    // )
-  }
+      {
+        (allCandidates)?
+        (
+          <table style={{width:'100%'}}>
+            <thead>
+              <tr>
+                <th>name</th>
+                <th>votes</th> 
+              </tr>
+            </thead>
+            <tbody>
+              {
+                (allCandidates).map((key, val) =>(
+                  <tr key={val}>
+                    <td>{key.name}</td>
+                    <td>{key.voteCount}</td>
+                  </tr>
+                ))
+              }
+            </tbody> 
+              
+          </table>
+            // messages.map((key, val) => (
+            //     // console.log(key.msg)
+            //     <div key={val} className="container">
+            //         <ul className="collapsible">
+            //             <Notification 
+            //                 message = {key.msg}
+            //                 time = {key.time}
+            //                 index = {val}
+            //             />
+            //         </ul>
+            //     </div>
+            // ))
+        )
+        :
+        (
+          null
+        )
+      }
+    </div>
+  )
 }
+
 
 export default App;
