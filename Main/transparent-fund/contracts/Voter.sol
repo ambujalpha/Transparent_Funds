1155192773d5dc4b1f6f452b7210b37b83933296
abhaykes1 <abhaykesharwani40@gmail.com>
2020-11-28 01:32:35 +0530

    updated voter smart contract with voting option

--- Main/transparent-fund/contracts/Voter.sol
+++ Main/transparent-fund/contracts/Voter.sol
@@ -7,18 +7,25 @@ contract Voter {
         string name;
         string designation;
         bool voted;
-        address votedTo;
+        string votedTo;
         address myAddress;
     }
     
     voter [] public voters;
     
     function setVoter(string memory _name, string memory _designation, address _voter) public {
-        voters.push(voter(_name, _designation, false, address(0), _voter ));
+        voters.push(voter(_name, _designation, false, '', _voter ));
     }
     
     function getNumVoter() public view returns(uint){
         return voters.length;
     }
-    
+    function voteTo(address _votedBy, string memory _votedTo) public {
+        for(uint i=0;i<voters.length; i++){
+            if(voters[i].myAddress == _votedBy){
+                voters[i].voted = true;
+                voters[i].votedTo = _votedTo;
+            }
+        }
+    }
 }
